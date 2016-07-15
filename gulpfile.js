a9e55eac528ab01c65394b4cf2e19216577e2a27
Asvae <ontrew@gmail.com>
2016-07-15 14:44:58 +0300

    Браузерифи вместо вебпака.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 var elixir = require('laravel-elixir')
-require('laravel-elixir-webpack-ex')
 
 elixir(function (mix) {
-    // mix.webpack('main.js')
-    // mix.webpack('api-tester.js', {}, 'app/ApiTester/resources/assets/build', 'app/ApiTester/resources/assets/js' )
-    mix.webpack('api-tester.js', {}, null, 'app/ApiTester/resources/assets/js')
-    mix.version([
-        'public/js/api-tester.js'
-    ])
+    mix.browserify('main.js')
 });
+
