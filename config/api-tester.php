5ad020ffda72e1ea35fb8d9411411aeeac9d9412
Asvae <ontrew@gmail.com>
2016-07-27 14:31:36 +0300

    Add configs.

---
+++ config/api-tester.php
@@ -0,0 +1,97 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable api tester
+    |--------------------------------------------------------------------------
+    |
+    | You can turn on and off the tester on will. Or maybe bind it to
+    | some env value.
+    |
+    */
+
+    'enabled'    => env('APP_DEBUG', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Middleware
+    |--------------------------------------------------------------------------
+    |
+    | Define list of middleware(s), that should be used for api-tester.
+    | CRSF token will be handled automatically.
+    |
+    */
+    'middleware' => ['web'],
+
+
+    'storage'      => [
+        'path' => 'api-tester',
+        'file' => 'requests.db',
+    ],
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Repositories
+    |--------------------------------------------------------------------------
+    |
+    | Specify class for RouteRepository and RequestRepository.
+    |
+    */
+    'repositories' => [
+        'routes'   => Asvae\ApiTester\Repositories\RouteLaravelRepository::class,
+        'requests' => Asvae\ApiTester\Repositories\RequestRepository::class
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default route
+    |--------------------------------------------------------------------------
+    |
+    | Define the route for api router.
+    | http://your-site.com/[route]
+    |
+    */
+    'route' => 'api-tester',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filter routes
+    |--------------------------------------------------------------------------
+    | All your routes will be filtered via given patterns. Both include and
+    | exclude are always applied. You can also use regex when needed.
+    |
+    | ## Examples
+    |
+    | ### Include all
+    | 'include' => [[]]
+    |
+    | ### Include some routes
+    | 'include' => [
+    |      'api/users',
+    |      'api/sales',
+    |      // ...
+    |  ]
+    |
+    | ### Include advanced
+    | 'include' => [
+    |      [
+    |         'path' => 'api/v(1|2|3)/.*',
+    |         'name' => '.*',
+    |         'method' => '(GET|POST|PUT|PATCH|DELETE)'
+    |      ],
+    |      // ...
+    |  ]
+    |
+    | ### Include all except 'users'
+    | 'include' => [[]],
+    | 'exclude' => ['api/users'],
+    |
+    */
+
+    'include' => [[]],
+
+    'exclude' => [],
+];
